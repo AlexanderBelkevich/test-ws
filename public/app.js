1c0b2b2e02fc2fe6cf1705d00d194d3d4fe7db70
Александр Белькевич <alexbelkevich@MacBook-Pro-Aleksandr.local>
2024-06-07 15:51:52 +0400

    fix url socket

--- public/app.js
+++ public/app.js
@@ -1,4 +1,4 @@
-const socket = io("ws://localhost:3500");
+const socket = io("https://test-ws-hmw3.onrender.com");
 
 const activity = document.querySelector(".activity");
 const msgInput = document.querySelector("input");
